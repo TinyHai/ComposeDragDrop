a58e1ba82a702c32dea04893f9409659c023f14d
TinyHai <1035908357@qq.com>
2023-08-17 13:33:03 +0800

    Add dropTarget Modifier

--- dragdrop/src/main/java/cn/tinyhai/compose/dragdrop/DropTarget.kt
+++ dragdrop/src/main/java/cn/tinyhai/compose/dragdrop/DropTarget.kt
@@ -3,49 +3,44 @@ package cn.tinyhai.compose.dragdrop
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.layout.onGloballyPositioned
+import cn.tinyhai.compose.dragdrop.modifier.dropTarget
 
 private const val TAG = "DropTarget"
+
+class DropTargetState<T> {
+    var isInBound by mutableStateOf(false)
+        internal set
+    var dataToDrop by mutableStateOf<T?>(null)
+        internal set
+
+    operator fun component1() = isInBound
+
+    operator fun component2() = dataToDrop
+
+    override fun toString(): String {
+        return "DropTargetState(isInBound=$isInBound, dataToDrop=$dataToDrop)"
+    }
+}
+
+@Composable
+fun <T> rememberDropTargetState() = remember {
+    DropTargetState<T>()
+}
 
 @Composable
 fun <T> DropTarget(
     onDrop: (T?) -> Unit,
     modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    dropTargetState: DropTargetState<T> = rememberDropTargetState(),
     content: @Composable BoxScope.(isInBound: Boolean, data: T?) -> Unit
 ) {
-    val boundInBox = remember {
-        mutableStateOf<Rect?>(null)
-    }
-    RegisterDropTarget(boundInBox, onDrop)
-
-    val dragDropState = LocalDragDrop.current
-    val isInBound by remember {
-        derivedStateOf {
-            boundInBox.value?.contains(dragDropState.calculateDragPosition()) ?: false
-        }
-    }
-    val data by remember {
-        derivedStateOf {
-            if (isInBound) {
-                dragDropState.dataToDrop as T?
-            } else {
-                null
-            }
-        }
-    }
-    Box(
-        modifier = modifier.onGloballyPositioned {
-            dragDropState.boundInBox(it).let { rect ->
-                boundInBox.value = rect
-            }
-        },
-    ) {
-        content(isInBound, data)
+    Box(modifier = modifier.dropTarget(dropTargetState, enabled, onDrop)) {
+        content(dropTargetState.isInBound, dropTargetState.dataToDrop)
     }
 }
---
+++ dragdrop/src/main/java/cn/tinyhai/compose/dragdrop/modifier/Modifiers.kt
@@ -0,0 +1,60 @@
+package cn.tinyhai.compose.dragdrop.modifier
+
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.layout.onGloballyPositioned
+import cn.tinyhai.compose.dragdrop.DropTargetState
+import cn.tinyhai.compose.dragdrop.LocalDragDrop
+import cn.tinyhai.compose.dragdrop.RegisterDropTarget
+import kotlinx.coroutines.flow.collectLatest
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Modifier.dropTarget(
+    state: DropTargetState<T>,
+    enabled: Boolean = true,
+    onDrop: (T?) -> Unit
+) =
+    composed {
+        val isEnabled by rememberUpdatedState(newValue = enabled)
+        val boundInBox = remember {
+            mutableStateOf<Rect?>(null)
+        }
+
+        if (isEnabled) {
+            RegisterDropTarget(boundInBox, onDrop)
+        }
+
+        val dragDropState = LocalDragDrop.current
+        LaunchedEffect(isEnabled) {
+            if (isEnabled) {
+                snapshotFlow {
+                    val isInBound =
+                        boundInBox.value?.contains(dragDropState.calculateDragPosition()) ?: false
+                    isInBound to (if (isInBound) dragDropState.dataToDrop as? T else null)
+                }.collectLatest { (isInBound, dataToDrop) ->
+                    state.apply {
+                        this.isInBound = isInBound
+                        this.dataToDrop = dataToDrop
+                    }
+                }
+            } else {
+                state.apply {
+                    isInBound = false
+                    dataToDrop = null
+                }
+            }
+        }
+
+        onGloballyPositioned {
+            dragDropState.boundInBox(it).let { rect ->
+                boundInBox.value = rect
+            }
+        }
+    }
