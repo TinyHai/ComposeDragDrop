b7b6356fbfad9c81cb9b086106b9c6eb3ffc8944
TinyHai <1035908357@qq.com>
2024-03-22 16:28:55 +0800

    using version catolog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-buildscript {
-    val agp_version by extra("8.0.2")
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.android.library") version "8.3.0" apply false
-    id("com.vanniktech.maven.publish") version "0.28.0" apply false
+    alias(libs.plugins.agp.app) apply false
+    alias(libs.plugins.agp.lib) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
